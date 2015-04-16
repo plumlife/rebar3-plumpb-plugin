d22854f8924b7154c14b01a49b3c456353045632
Parnell Springmeyer <parnell@digitalmentat.com>
2015-04-16 17:19:44 -0500

    Adding better compilation support

--- src/provider_plumpb.erl
+++ src/provider_plumpb.erl
@@ -4,7 +4,7 @@
 -export([init/1, do/1, format_error/1]).
 
 -define(PROVIDER, plumpb).
--define(DEPS, [install_deps]).
+-define(DEPS, [install_deps, compile]).
 
 %% ===================================================================
 %% Public API
